0ee824dad08707d4f95cb1d0772ed9cbbdc2860e
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-01-27 15:35:33 +0530

    Give grade by checking the marks

---
+++ decision control/if elif else/grade.py
@@ -0,0 +1,13 @@
+m= int(input("Enter marks: "))
+if 90<m<=100:
+    print("Gradce A")
+elif 80<m<=90:
+    print("Grade B")
+elif 70<m<=80:
+    print("Grade c")
+elif 60<m<=70:
+    print("Grade D")
+elif 50<m<=60:
+    print("Grade E")
+else:
+    print("Grade F")
