c95eda87eaf1fc26c90572b41da7b2b657a90277
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-01-27 15:57:21 +0530

    Case Statement

---
+++ decision control/matchcase.py
@@ -0,0 +1,13 @@
+x= int(input("Enter a number: "))
+match x:
+    case 1:
+        print("One")
+    case 2:
+        print("Two")
+    case 3:
+        print("Three")
+    case 4:
+        print("Four")
+    case _:
+        print("Default")
+print()
