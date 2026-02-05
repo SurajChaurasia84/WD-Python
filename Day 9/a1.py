0b43b7f11312889be6eb33af70c421551cd726b6
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-02-05 15:50:27 +0530

    Day 9

---
+++ Day 9/a1.py
@@ -0,0 +1,24 @@
+# a=input("Enter something: ")
+# b=hash(a)
+
+# print("Hash value of",a,"is",b)
+
+# print(str(a)
+
+# from keyword import kwlist
+# print(kwlist)
+
+# from calendar import month 
+# mm = int(input("Enter month:"))
+# yy = int(input("Enter year:"))
+# print(month(yy,mm))
+
+
+import calendar
+mm=int(input("Enter month : "))
+yy=int(input("Enter year : "))
+print(calendar.month(yy,mm))
+
+
+y = str("Enter your name: ")
+print(type(y))
